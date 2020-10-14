812bcecf0152bd43d6ac9fc64ae2efcbccda2643
dengzhiqiang <dengzhiqiang@agree.com.cn>
2020-10-14 10:27:45 +0800

    [feature] build project

---
+++ cloud-provider-payment8001/src/main/java/com/dzq/springcloud/controller/PaymentController.java
@@ -0,0 +1,45 @@
+package com.dzq.springcloud.controller;
+
+import com.dzq.springcloud.entities.CommonResult;
+import com.dzq.springcloud.entities.Payment;
+import com.dzq.springcloud.service.IPaymentService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Slf4j
+public class PaymentController {
+
+    @Autowired
+    private IPaymentService paymentService;
+
+    @PostMapping(value = "/payment/create")
+    public CommonResult create(@RequestBody Payment payment) {
+        int result = paymentService.create(payment);
+        String code, message;
+        if (result > 0) {
+            code = "200";
+            message = "新增成功";
+        } else {
+            code = "400";
+            message = "新增失败";
+        }
+        return new CommonResult(code, message);
+    }
+
+    @GetMapping(value = "/payment/get/{id}")
+    public CommonResult getPaymentById(@PathVariable("id") Long id) {
+        Payment payment = paymentService.getPaymentById(id);
+
+        String code, message;
+        if (null != payment) {
+            code = "200";
+            message = "查询成功";
+        } else {
+            code = "400";
+            message = "查询失败";
+        }
+        return new CommonResult(code, message, payment);
+    }
+}
